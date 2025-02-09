2399e5ca162b6394e59025f585854906b845a264
Alberto <alzel@banhcafe.hn>
2025-02-09 17:25:22 -0600

    fix: update url portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,30 @@
+import { getImageUrl } from "../../utils";
+import styles from "./Navbar.module.css";
+
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  console.log(getImageUrl("nav/menuIcon.png"));
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img src="../../assets/nav/menuIcon.png" alt="menu-icon" />
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Exaperience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
