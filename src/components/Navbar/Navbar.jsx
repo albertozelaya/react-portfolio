ed2e20d04b210a5bcba2875740f7b2c6f6dbef52
Alberto <albertolaniman@gmail.com>
2025-02-17 21:46:33 -0600

    fix: correct function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const NavBar = () => {
         Portfolio
       </a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="" />
+        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button" />
         <ul className={styles.menuItems}>
           <li>
             <a href="#about">About</a>
