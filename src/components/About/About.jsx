499eba78cdfe3fb8c416d1b900e5a4971ee5c24f
Alberto <alzel@banhcafe.hn>
2025-03-12 23:57:06 -0600

    feat: add about module

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,25 @@
 import getImageUrl from "../../utils";
+import styles from "./About.module.css";
 
 export const About = () => {
   return (
-    <section>
-      <h2>About</h2>
-      <div className="">
-        <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" />
-        <ul>
-          <li>
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage} />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>Im a Frontend developer with experience in building responsive and optimized sites</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="ui icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UX Designer</h3>
+              <p>I have experience developing UX Designs on Figma</p>
             </div>
           </li>
         </ul>
