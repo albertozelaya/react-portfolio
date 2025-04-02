6be6d25afb3fcef69576bd59e19bf57273625b88
Alberto <albertolaniman@gmail.com>
2025-04-02 05:31:36 -0600

    feat: add new experience part

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,17 +1,18 @@
 import skills from "../../../src/data/skills.json";
 import history from "../../../src/data/history.json";
 import getImageUrl from "../../utils";
+import styles from "./Experience.module.css";
 
 export const Experience = () => {
   return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div>
-        <div>
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
-              <div key={id}>
-                <div>
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
                   <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                 </div>
                 <p>{skill.title}</p>
@@ -19,14 +20,19 @@ export const Experience = () => {
             );
           })}
         </div>
-        <ul>
+        <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
-              <li key={id}>
+              <li key={id} className={styles.historyItem}>
                 <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
-                <div>
+                <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
                 </div>
               </li>
             );
